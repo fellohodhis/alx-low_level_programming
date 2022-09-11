ddcde2aabeb5bb958033a07db006b07ad44d7d93
fellohodhis <odhiambof446@gmail.com>
2022-09-11 11:00:50 -0700

    Task 5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
-* main - print the string in the put function
-*
-* Description: using the main function 
-* this program prints "Programming is like building a mutilingual puzzle
-* Return: o
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
