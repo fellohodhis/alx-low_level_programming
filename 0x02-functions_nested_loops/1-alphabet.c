9b8699a4661446131005ceb479a7955bfcafeea4
fellohodhis <odhiambof446@gmail.com>
2022-09-13 06:14:44 -0700

    Task 1. Lowercase alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet: Prints lowercase alphabets
- */
-
+  * print_alphabet - Make the alphabet
+  *
+  * Return: void
+  */
 void print_alphabet(void)
 {
-	int j;
+	char c;
 
-		for (j = 'a'; j <= 'z'; j++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-	    	_putchar(j);
+		_putchar(c);
 	}
+
 	_putchar('\n');
 }
