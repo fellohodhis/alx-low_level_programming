36f211cfe4a1133af7c111a4785638b43035ca83
fellohodhis <odhiambof446@gmail.com>
2022-09-19 04:56:33 -0700

    Task 1. swaping values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - sawp the value of two integers
+ *
+ * @a: first interger to be swapped
+ * @b: second integer to be swapped
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = te;
+}
