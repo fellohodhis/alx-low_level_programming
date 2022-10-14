ea372a2a0d87bccbd2658ed70f27f83fbafd8ce9
fellohodhis <odhiambof446@gmail.com>
2022-10-14 02:48:05 -0700

    Header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -19,4 +19,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endif
+#endif /* _VARIADIC_FUNCTIONS_H_ */
